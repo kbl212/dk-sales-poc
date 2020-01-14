a2e9ac625f7c012388776bf501fe468bb190d8ac
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2020-01-13 22:13:52 -0500

    added shop page, collection-preview component and collection-item components.

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './collection-item.styles.scss';
+
+const collectionItem = ({ id, name, price, imageUrl }) => (
+  <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='collection-footer'>
+      <div className='name'>
+        { name }
+      </div>
+      <div className='price'>
+        { price }
+      </div>
+    </div>
+  </div>
+)
+
+export default collectionItem;
