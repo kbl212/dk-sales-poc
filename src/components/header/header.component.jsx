0e587149308b9c18c512b564a9641d8f641d41db
kbl212 <kyle.lauritzen@gmail.com>
2020-01-13 23:31:38 -0500

    added header component, with logo and options

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+
+const Header = () => (
+  <div className='header'>
+  <Link className='logo-container' to="/">
+    <img src={require('../../assets/dk_logo_3.png')} alt="logo" className='logo' />
+  </Link>
+  <div className='options'>
+    <Link className='option' to='/shop'>
+      SHOP
+    </Link>
+    <Link className='option' to='/shop'>
+      CONTENT
+    </Link>
+  </div>
+  </div>
+)
+
+export default Header;
