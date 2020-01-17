3af8e46928bef3232ccb7a54c67d5bc8715b853b
kbl212 <kyle.lauritzen@gmail.com>
2020-01-16 22:39:11 -0500

    added cart-icon and cart-dropdown components. also added cart reducer.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,32 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
-  <Link className='logo-container' to="/">
-    <img src={require('../../assets/dk_logo_3.png')} alt="logo" className='logo' />
-  </Link>
-  <div className='options'>
-    <Link className='option' to='/shop'>
-      SHOP
+    <Link className='logo-container' to="/">
+      <img src={require('../../assets/dk_logo.png')} alt="logo" className='logo' />
     </Link>
-    <Link className='option' to='/shop'>
-      CONTENT
-    </Link>
-    {
-      currentUser ? (
-      <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
-      :
-      (<Link className='option' to='/signIn'>SIGN IN</Link>)
-    }
-  </div>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTENT
+      </Link>
+      {
+        currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
+        :
+        (<Link className='option' to='/signIn'>SIGN IN</Link>)
+      }
+      <CartIcon />
+    </div>
+    { hidden ? null : <CartDropdown /> }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
