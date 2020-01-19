18de8f5772b7c7e04c3087afff9cf28596b624ae
kbl212 <kyle.lauritzen@gmail.com>
2020-01-18 23:26:21 -0500

    added cart persistence in localStorage through redux-persist library.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden, selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors'
 import './header.styles.scss';
 
