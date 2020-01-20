1b7511476c9f230cef35884a290c53c9889475b5
kbl212 <kyle.lauritzen@gmail.com>
2020-01-20 00:22:46 -0500

    added Stripe API integration. Change site title.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100; // price must be in cents
+  const publishableKey = 'pk_test_Ld5R4f4Vx4bqkDbS8IxzVRBG00PArCLc8V';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='DK Sales'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
